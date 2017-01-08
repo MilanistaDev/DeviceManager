c9e81bc45e66a79c909ee1acebb2c1e9129dcb41
Takuya Aso <milanista224@me.com>
2017-01-09 03:25:21 +0900

    [DeviceManager]Designed Main Screen and add property

--- DeviceManager/ViewController.swift
+++ DeviceManager/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var modelNumberLabel: UILabel!
+    @IBOutlet weak var useriOSDeviceLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func checkDeviceAction(_ sender: Any) {
+    }
+
+    @IBAction func CorrespondingDeviceCheck(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
